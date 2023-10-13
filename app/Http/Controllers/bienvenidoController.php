23c5d507e6c600800320ea7cf8cd9949c917d151
Kyan Camacho <kyan.camacho21@cetis107.edu.mx>
2023-10-12 19:49:58 -0600

    V.1.0.1

--- app/Http/Controllers/bienvenidoController.php
+++ app/Http/Controllers/bienvenidoController.php
@@ -9,10 +9,10 @@ class bienvenidoController extends Controller
     public function bienvenido(){
         $nombre = "Kyan";
 
-        $alumnos = ['Skibidi','Garden Of','Toilet','Ban Ban'];
+        $alumnos = ['Skibidi','Garten Of','Toilet','Ban Ban'];
 
         $datos = array(['nombre' => 'Skibidi', 'edad'=>17, 'turno' => 'Vespertino'],
-                       ['nombre' => 'Garden Of', 'edad'=>16, 'turno' => 'Matutino'],
+                       ['nombre' => 'Garten Of', 'edad'=>16, 'turno' => 'Matutino'],
                        ['nombre' => 'Toilet', 'edad'=>15, 'turno' => 'Vespertino'],
                        ['nombre' => 'Ban Ban', 'edad'=>14, 'turno' => 'Matutino']);
 
